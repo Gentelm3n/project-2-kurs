a7d9b7f8bf8a37ca77a83f64070877423dde847b
GentelmenFrUK <kovril69@gmail.com>
2025-09-14 17:24:05 +0300

    Create First Form
    Создал форму, попробовал работу календарей и ввод строк.

--- AppForDesktop/AppForDesktop/Form1.Designer.cs
+++ AppForDesktop/AppForDesktop/Form1.Designer.cs
@@ -28,13 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.FirstButton = new System.Windows.Forms.Button();
+            this.ButtonClick = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.submitBut = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // FirstButton
+            // 
+            this.FirstButton.Location = new System.Drawing.Point(30, 2);
+            this.FirstButton.Name = "FirstButton";
+            this.FirstButton.Size = new System.Drawing.Size(99, 36);
+            this.FirstButton.TabIndex = 3;
+            this.FirstButton.Text = "Show Calendar";
+            this.FirstButton.UseVisualStyleBackColor = true;
+            this.FirstButton.Click += new System.EventHandler(this.show_and_hidden_Calendar);
+            // 
+            // ButtonClick
+            // 
+            this.ButtonClick.Location = new System.Drawing.Point(30, 369);
+            this.ButtonClick.Name = "ButtonClick";
+            this.ButtonClick.Size = new System.Drawing.Size(166, 57);
+            this.ButtonClick.TabIndex = 0;
+            this.ButtonClick.Text = "Click me";
+            this.ButtonClick.UseVisualStyleBackColor = true;
+            this.ButtonClick.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 335);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(30, 41);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(514, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(514, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Put subject";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // submitBut
+            // 
+            this.submitBut.Location = new System.Drawing.Point(514, 68);
+            this.submitBut.Name = "submitBut";
+            this.submitBut.Size = new System.Drawing.Size(75, 23);
+            this.submitBut.TabIndex = 6;
+            this.submitBut.Text = "Submit";
+            this.submitBut.UseVisualStyleBackColor = true;
+            this.submitBut.Click += new System.EventHandler(this.submit_button);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(389, 353);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.submitBut);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.FirstButton);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ButtonClick);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ButtonClick;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button FirstButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button submitBut;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
 
--- AppForDesktop/AppForDesktop/Form1.cs
+++ AppForDesktop/AppForDesktop/Form1.cs
@@ -15,6 +15,38 @@ namespace AppForDesktop
         public Form1()
         {
             InitializeComponent();
+            monthCalendar1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello Debils!";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void show_and_hidden_Calendar(object sender, EventArgs e)
+        {
+            monthCalendar1.Visible = !monthCalendar1.Visible;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submit_button(object sender, EventArgs e)
+        {
+            string subject = textBox1.Text;
+            label2.Text = subject;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
